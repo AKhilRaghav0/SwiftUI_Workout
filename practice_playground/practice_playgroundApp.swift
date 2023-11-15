7dc8ab7830da9e092d285401fd3ce0f49ab61d94
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-15 22:13:39 +0530

    Initial Commit

---
+++ practice_playground/practice_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  practice_playgroundApp.swift
+//  practice_playground
+//
+//  Created by Akhil on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct practice_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
