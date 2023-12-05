6cda92200e715aa054bcb89f3f68dc28e9bd6c83
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-12-05 17:02:09 +0530

    Update practice_playgroundApp.swift

--- practice_playground/practice_playgroundApp.swift
+++ practice_playground/practice_playgroundApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct practice_playgroundApp: App {
     var body: some Scene {
         WindowGroup {
-            MusicPlayer(expandedSheet: .constant(true), animatoin: Namespace().wrappedValue)
+//            MusicPlayer(expandedSheet: .constant(true), animatoin: Namespace().wrappedValue)
                
         }
     }
